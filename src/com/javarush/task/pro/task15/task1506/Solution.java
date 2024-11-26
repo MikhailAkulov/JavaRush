61f3944df6669200abfd3e6dea5fbae8db17e54a
Akuloff.M <Akuloff.M@gmail.com>
2024-11-26 23:50:42 +0300

    updated 1506 task

--- src/com/javarush/task/pro/task15/task1506/Solution.java
+++ src/com/javarush/task/pro/task15/task1506/Solution.java
@@ -16,11 +16,34 @@ public class Solution {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             List<String> list = Files.readAllLines(Path.of(sc.nextLine()));
-
-            // C:\Users\Akulo\Desktop\log.txt
+            for (int i = 0; i < list.size(); i++) {
+                String str = list.get(i);
+                for (int j = 0; j < str.length(); j++) {
+                    Character ch = str.charAt(j);
+                    if (ch == ' ' || ch == ',' || ch == '.') {
+                        continue;
+                    }
+                    System.out.print(ch);
+                }
+            }
         } catch (IOException e) {
             System.out.println("catch IOException " + e);
         }
+
+        // вариант 2
+//        try (Scanner scanner = new Scanner(System.in)) {
+//            List<String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
+//            lines.forEach(str -> {
+//                char[] chars = str.toCharArray();
+//                for (char character : chars) {
+//                    if (character != ' ' && character != '.' && character != ',') {
+//                        System.out.print(character);
+//                    }
+//                }
+//            });
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong : " + e);
+//        }
     }
 }
 
