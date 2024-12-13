de15b3a86dad05b87f672b7af87914ddb6ba8a4a
Akuloff.M <Akuloff.M@gmail.com>
2024-12-13 21:04:38 +0300

    updated JavaCore package, started 2 level

---
+++ src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 128;
+        Integer y = 256;
+        print(x);
+        print(y);
+    }
+
+    public static void print(int a) {
+    }
+    public static void print(Integer b) {
+    }
+}
