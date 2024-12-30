10b78190213bbbe663e5249ea02c8b21e5e5012e
Akuloff.M <Akuloff.M@gmail.com>
2024-12-30 19:49:15 +0300

    updated JavaCore package 6 level

--- src/com/javarush/task/task16/task1607/Solution.java
+++ src/com/javarush/task/task16/task1607/Solution.java
@@ -25,7 +25,14 @@ public class Solution {
 
     public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
         int finishedCount = 0;
-        //напишите тут ваш код
+        for (Horse horse : horses) {
+            if (!horse.isFinished()) {
+                System.out.println("Waiting for " + horse.getName());
+                horse.join();
+            } else {
+                finishedCount++;
+            }
+        }
         return finishedCount;
     }
 
