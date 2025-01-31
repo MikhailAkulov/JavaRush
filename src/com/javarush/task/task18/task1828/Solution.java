37c264fc81b1bb407935174bbea0fbe8c7372b95
Akuloff.M <Akuloff.M@gmail.com>
2025-01-31 20:31:12 +0300

    updated JavaCore package 8 level

--- src/com/javarush/task/task18/task1828/Solution.java
+++ src/com/javarush/task/task18/task1828/Solution.java
@@ -40,7 +40,158 @@ id productName price quantity
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static class Product {
+        int id;
+        String name;
+        String price;
+        String quantity;
 
+        public Product(int id, String name, String price, String quantity) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%-8d%-30s%-8s%-4s", id, name, price, quantity);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            return;
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        List<Product> products = new ArrayList<>();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                Product product = getProduct(fileReader.readLine());
+                products.add(product);
+            }
+        }
+
+        boolean fileHaveData;
+        if (products.isEmpty()) {
+            fileHaveData = false;
+        } else {
+            fileHaveData = true;
+        }
+
+        switch (args[0]) {
+            case "-c":
+                int id = 0;
+                for (Product product : products) {
+                    if (product.id > id) {
+                        id = product.id;
+                    }
+                }
+                String name = "";
+                for (int i = 1; i < args.length - 2; i++) {
+                    name += args[i] + " ";
+                }
+                if (name.length() > 30) {
+                    name = name.substring(0, 30);
+                }
+                String price = args[args.length - 2];
+                if (price.length() > 8) {
+                    price = price.substring(0, 8);
+                }
+                String quantity = args[args.length - 1];
+                if (quantity.length() > 4) {
+                    quantity = quantity.substring(0, 4);
+                }
+                Product product = new Product(++id, name.trim(), price, quantity);
+                try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+                    if (fileHaveData) {
+                        fileWriter.write("\n");
+                        fileWriter.write(product.toString());
+                    } else {
+                        fileWriter.write(product.toString());
+                    }
+                }
+                break;
+            case "-d":
+                if (!fileHaveData) {
+                    System.out.println("Файл пуст, нет данных для удаления");
+                } else {
+                    int idForDeleted = Integer.parseInt(args[1]);
+                    for (int i = 0; i < products.size(); i++) {
+                        Product currentProduct = products.get(i);
+                        if (currentProduct.id == idForDeleted) {
+                            products.remove(currentProduct);
+                        }
+                    }
+                    try (FileWriter fileWriter = new FileWriter(fileName)) {
+                        for (int i = 0; i < products.size(); i++) {
+                            Product currentProduct = products.get(i);
+                            if (i < products.size() - 1) {
+                                fileWriter.write(currentProduct.toString());
+                                fileWriter.write("\n");
+                            } else {
+                                fileWriter.write(currentProduct.toString());
+                            }
+                        }
+                    }
+                }
+                break;
+            case "-u":
+                int idForRefresh = Integer.parseInt(args[1]);
+                int index = -1;
+                for (Product p : products) {
+                    if (p.id == idForRefresh) {
+                        idForRefresh = p.id;
+                        index = products.indexOf(p);
+                    }
+                }
+                String nameForRefresh = "";
+                for (int i = 2; i < args.length - 2; i++) {
+                    nameForRefresh += args[i] + " ";
+                }
+                if (nameForRefresh.length() > 30) {
+                    nameForRefresh = nameForRefresh.substring(0, 30);
+                }
+                String priceForRefresh = args[args.length - 2];
+                if (priceForRefresh.length() > 8) {
+                    priceForRefresh = priceForRefresh.substring(0, 8);
+                }
+                String quantityForRefresh = args[args.length - 1];
+                if (quantityForRefresh.length() > 4) {
+                    quantityForRefresh = quantityForRefresh.substring(0, 4);
+                }
+                Product productForRefresh = new Product(idForRefresh, nameForRefresh.trim(), priceForRefresh, quantityForRefresh);
+                products.set(index, productForRefresh);
+                try (FileWriter fileWriter = new FileWriter(fileName)) {
+                    for (int i = 0; i < products.size(); i++) {
+                        Product currentProduct = products.get(i);
+                        if (i < products.size() - 1) {
+                            fileWriter.write(currentProduct.toString());
+                            fileWriter.write("\n");
+                        } else {
+                            fileWriter.write(currentProduct.toString());
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    public static Product getProduct(String string) {
+        String id = string.substring(0, 8).trim();
+        String name = string.substring(8, 38).trim();
+        String price = string.substring(38, 46).trim();
+        String quantity = string.substring(46, 50).trim();
+        return new Product(Integer.parseInt(id), name, price, quantity);
     }
 }
+
+// C:\Users\Akulo\Desktop\SourceDirectory\sourceFileCRUD.txt
+// -c Каска для сноубордистов, размером с дом 512.00 32
+// -d 19847985
+// -u 19847987 Каска для лыжников с проблесковым маячком 1024.99 128
+// -u 3 Каска для лыжников с проблесковым маячком 1024.99 128
