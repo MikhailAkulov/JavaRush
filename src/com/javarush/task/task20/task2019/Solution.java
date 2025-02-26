60b2da45e1b2c6ed265df6e36dbce23e537560f3
Akuloff.M <Akuloff.M@gmail.com>
2025-02-26 20:18:07 +0300

    updated JavaCore package 10 level added extraExample

---
+++ src/com/javarush/task/task20/task2019/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task20.task2019;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Исправить ошибку. Сериализация
+
+После десериализации объекта класса Solution обнаружили, что данных в словаре [m] нет :(
+*/
+
+public class Solution implements Serializable {
+
+    public static void main(String args[]) throws Exception {
+        FileOutputStream fileOutput = new FileOutputStream("C:\\Users\\Akulo\\Desktop\\Task2019.txt");
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+
+        Solution solution = new Solution();
+        outputStream.writeObject(solution);
+
+        fileOutput.close();
+        outputStream.close();
+
+        //load
+        FileInputStream fiStream = new FileInputStream("C:\\Users\\Akulo\\Desktop\\Task2019.txt");
+        ObjectInputStream objectStream = new ObjectInputStream(fiStream);
+
+        Solution loadedObject = (Solution) objectStream.readObject();
+
+        fiStream.close();
+        objectStream.close();
+
+        //Attention!!
+        System.out.println(loadedObject.size());
+    }
+
+    private Map<String, String> m = new HashMap<>();
+
+    public Map<String, String> getMap() {
+        return m;
+    }
+
+    public Solution() {
+        m.put("Mickey", "Mouse");
+        m.put("Mickey", "Mantle");
+    }
+
+    public int size() {
+        return m.size();
+    }
+
+}
